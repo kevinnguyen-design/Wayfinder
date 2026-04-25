ef24b6ce54fa15c3da410cdf7d3f3a8e342431e2
Kevin Nguye <kevinnql2004@gmail.com>
2026-04-25 19:02:13 -0400

    feat: add BLE service and characteristic

--- Wayfinder.ino
+++ Wayfinder.ino
@@ -1,6 +1,37 @@
+#include <ArduinoBLE.h>
+
+const long BAUD_RATE = 115200;
+BLEService navService("19B10000-E8F2-537E-4F6C-D104768A1214");
+BLEByteCharacteristic navCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(BAUD_RATE);
+  while (!Serial); // Optional: wait for serial port to connect
+
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+  
+  BLE.setLocalName("WAYFINDER-CORE");
+  BLE.setAdvertisedService(navService);
+  navService.addCharacteristic(navCharacteristic);
+  BLE.addService(navService);
+  navCharacteristic.writeValue(0);
+  BLE.advertise();
+  
+  Serial.println("BLE Peripheral Ready");
 }
 
 void loop() {
+  BLEDevice central = BLE.central();
+  if (central) {
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
+    while (central.connected()) {
+      // Loop here
+    }
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
 }
