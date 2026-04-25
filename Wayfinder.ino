82dfcf1f0d88127758eec798084b6f3c1fa63f32
Kevin Nguye <kevinnql2004@gmail.com>
2026-04-25 19:10:46 -0400

    feat: implement haptic command logic

--- Wayfinder.ino
+++ Wayfinder.ino
@@ -3,8 +3,11 @@
 #include <Adafruit_DRV2605.h>
 
 const long BAUD_RATE = 115200;
-BLEService navService("19B10000-E8F2-537E-4F6C-D104768A1214");
-BLEByteCharacteristic navCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+const char* SERVICE_UUID = "19B10000-E8F2-537E-4F6C-D104768A1214";
+const char* CHAR_UUID = "19B10001-E8F2-537E-4F6C-D104768A1214";
+
+BLEService navService(SERVICE_UUID);
+BLEByteCharacteristic navCharacteristic(CHAR_UUID, BLERead | BLEWrite);
 Adafruit_DRV2605 drv;
 
 void setup() {
@@ -32,12 +35,41 @@ void setup() {
   drv.setMode(DRV2605_MODE_INTTRIG); 
 }
 
+void processCommand(uint8_t cmd) {
+  switch (cmd) {
+    case 0x01: // Left
+      drv.setWaveform(0, 11); // Sharp Click - 100%
+      drv.setWaveform(1, 0);  // end waveform
+      drv.go();
+      Serial.println("Haptic: Left");
+      break;
+    case 0x02: // Right
+      drv.setWaveform(0, 11);
+      drv.setWaveform(1, 11);
+      drv.setWaveform(2, 0);
+      drv.go();
+      Serial.println("Haptic: Right");
+      break;
+    case 0x03: // Arrived
+      drv.setWaveform(0, 15); // Long buzz
+      drv.setWaveform(1, 0);
+      drv.go();
+      Serial.println("Haptic: Arrived");
+      break;
+    default:
+      break;
+  }
+}
+
 void loop() {
   BLEDevice central = BLE.central();
   if (central) {
     Serial.print("Connected to central: ");
     Serial.println(central.address());
     while (central.connected()) {
+      if (navCharacteristic.written()) {
+        processCommand(navCharacteristic.value());
+      }
       delay(10); // Yield to background tasks
     }
     Serial.print("Disconnected from central: ");
